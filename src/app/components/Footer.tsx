a334b927c5989b155bbe2576edb7b141ce227d12
Kurt Gray <kurtgray@gmail.com>
2023-10-29 10:57:17 -0700

    mobile styles

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -6,34 +6,34 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 export default function Footer() {
   return (
     <div className="footer">
-      {/* <div className="header__links"> */}
-      <Link href="#home" className="header__link">
-        Home
-      </Link>
-      <Link href="#about-me" className="header__link">
-        About
-      </Link>
-      <Link href="#projects" className="header__link">
-        Projects
-      </Link>
-      <Link href="#contact" className="header__link">
-        Contact
-      </Link>
-      <Link
-        target="_blank"
-        className="header__icon"
-        href="https://github.com/kurtmgray"
-      >
-        <GitHubIcon />
-      </Link>
-      <Link
-        target="_blank"
-        className="header__icon"
-        href="https://www.linkedin.com/in/kurtmgray"
-      >
-        <LinkedInIcon />
-      </Link>
-      {/* </div> */}
+      <div className="header__links">
+        <Link href="#home" className="header__link">
+          Home
+        </Link>
+        <Link href="#about-me" className="header__link">
+          About
+        </Link>
+        <Link href="#projects" className="header__link">
+          Projects
+        </Link>
+        <Link href="#contact" className="header__link">
+          Contact
+        </Link>
+        <Link
+          target="_blank"
+          className="header__icon"
+          href="https://github.com/kurtmgray"
+        >
+          <GitHubIcon />
+        </Link>
+        <Link
+          target="_blank"
+          className="header__icon"
+          href="https://www.linkedin.com/in/kurtmgray"
+        >
+          <LinkedInIcon />
+        </Link>
+      </div>
     </div>
   );
 }
